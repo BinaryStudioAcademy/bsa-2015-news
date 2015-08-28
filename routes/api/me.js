a971f217371912677c266457ca19892a153737df
voby <its.voby@gmail.com>
2015-08-28 09:44:54 +0300

    Logout fix

--- routes/api/me.js
+++ routes/api/me.js
@@ -1,4 +1,16 @@
+var jsonwebtoken = require('jsonwebtoken');
+var Cookies = require('cookies');
+
 module.exports = function(app){
+	app.get('/api/logout', function(req, res) {
+		var current_url = req.protocol + '://' + req.get('host') + req.url;
+
+		var cookies = new Cookies(req, res);
+		cookies.set('referer', current_url);
+		cookies.set('x-access-token');
+		res.redirect('/');
+	});
+
 	app.get('/api/me', function(req, res) {
 		res.send(req.decoded);
 	});
