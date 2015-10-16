cb6f22d198b716ba8970484614185fe96198e06d
voby <its.voby@gmail.com>
2015-10-16 16:16:50 +0300

    Post likes debug v2

--- frontend/javascripts/news/NewsController.js
+++ frontend/javascripts/news/NewsController.js
@@ -301,6 +301,9 @@ vm.switchTab = function(url) {
 	};
 
 	vm.newsLike = function(newsId, userId, index) {
+		console.log("Da newsId", newsId);
+		console.log("Da userId", userId);
+		console.log("Da index", index);
 		if(vm.posts[index].likes.indexOf(userId) < 0) {
 			NewsService.newsLike(newsId, userId).then(function() {
 				socket.emit("like post", {post: newsId, user: userId, isLike: true});
