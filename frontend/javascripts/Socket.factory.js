15a2ffa3156a138592f041eed20c604f49ccd8b7
Anatoliy Sieryi <jacobator@gmail.com>
2016-01-20 14:10:21 +0200

    prod ver

--- frontend/javascripts/Socket.factory.js
+++ frontend/javascripts/Socket.factory.js
@@ -5,7 +5,7 @@ SocketFactory.$inject = ['socketFactory'];
 
 function SocketFactory(socketFactory) {
     return socketFactory({
-        ioSocket: io.connect('http://localhost:4035')
-        //ioSocket: io.connect('http://216.86.147.185:4035')
+        //ioSocket: io.connect('http://localhost:4035')
+        ioSocket: io.connect('http://216.86.147.185:4035')
     });
 }
